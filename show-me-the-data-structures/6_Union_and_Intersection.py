0a572668ad9f5cb7b15bc069bbdb63ef4c32e25a
rene angele <reneang17@gmail.com>
2020-04-01 07:10:32 -0600

    Trvial rename

--- show-me-the-data-structures/6_Union_and_Intersection.py
+++ show-me-the-data-structures/6_Union_and_Intersection.py
@@ -136,6 +136,20 @@ def intersection(llist_1, llist_2):
 if __name__ == '__main__':
     # Aux test functions
 
+# Test case 1
+
+    linked_list_1 = LinkedList()
+    linked_list_2 = LinkedList()
+
+    element_1 = [3,2,4,35,6,65,6,4,3,21]
+    element_2 = [6,32,4,9,6,1,11,21,1]
+
+    for i in element_1:
+        linked_list_1.append(i)
+
+    for i in element_2:
+        linked_list_2.append(i)
+
     print (union(linked_list_1,linked_list_2))
     print (intersection(linked_list_1,linked_list_2))
 
